18b00af63517578d2255befe9f9329a0d862faa4
Sergey <sdzhagov@gmail.com>
2024-11-26 21:25:01 +0700

    Add bot replies before deleting message

--- main.py
+++ main.py
@@ -8,14 +8,15 @@ BASE_URL = f'https://api.telegram.org/bot{API}/'
 BANNED_WORDS = {'хуй'}
 
 def check_banned_word(text):
-    return any(word in text for word in BANNED_WORDS)
+    status = any(word in text for word in BANNED_WORDS)
+    return status
 
 
 class SpamDetector:
     def __init__(self):
         pass
-    def is_spam(self, text):
 
+    def is_spam(self, text):
         return random() < 0.5
 
 
@@ -25,10 +26,14 @@ class Bot:
         self.chat_id = chat_id
         self.base_url = f'https://api.telegram.org/bot{API}/'
         self.spam_detector = SpamDetector()
+        self.processed_messages = []
+        self.last_update_id = None
 
     def _get_updates(self):
         url = self.base_url + 'getUpdates'
-        params = {'offset': None, 'timeout': 30}
+        params = {'timeout': 1}
+        if self.last_update_id:
+            params['offset'] = self.last_update_id + 1
         response = requests.get(url, params=params)
         return response.json()
 
@@ -37,21 +42,28 @@ class Bot:
         params = {'chat_id': self.chat_id, 'message_id': message_id}
         requests.post(url, params=params)
 
+    def _send_message(self, message_id):
+        url = self.base_url + 'sendMessage'
+        text = 'я тебя сейчас удалю нафиг'
+        params = {'chat_id': self.chat_id, 'text': text, 'reply_to_message_id': message_id}
+        requests.post(url, params=params)
+
     def process_messages(self):
         while True:
             updates = self._get_updates()
             for update in updates.get('result', []):
+                self.last_update_id = update['update_id']
                 message = update.get('message')
-                if message and 'text' in message:
+                if message and 'text' in message and message['message_id'] not in self.processed_messages:
                     text = message['text'].lower()
-                    if self.spam_detector.is_spam(text) or check_banned_word(text):
+                    if check_banned_word(text):
+                        self._send_message(message['message_id'])
                         self._delete_message(message['message_id'])
+                        self.processed_messages.append(message['message_id'])
 
 
 
 if __name__ == "__main__":
     bot = Bot(API, CHAT_ID)
     bot.process_messages()
-
-
 
