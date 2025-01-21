99f1242aee1ebbca1ed9ec8b6ab17461ffd5d677
sergey <12sdkskdjskdjksdj@gmail.com>
2025-01-21 22:32:36 +0300

    Bot link fix

--- app/keyboards.py
+++ app/keyboards.py
@@ -4,6 +4,6 @@ add_button = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(
             text='Добавить бота в чат',
-            url='https://t.me/antishlyuh800bot?startgroup&admin=promote_members+delete_messages+restrict_members+invite_users+pin_messages+manage_video_chats'
+            url='https://t.me/antispam_ai_light_bot?startgroup&admin=promote_members+delete_messages+restrict_members+invite_users+pin_messages+manage_video_chats'
         )]
     ])
